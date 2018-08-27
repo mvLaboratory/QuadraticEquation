915dfd2195548e5ce5980e974afb39c46688f3c3
Volodymyr Mykytiuk <mvlabaratory@gmail.com>
2018-08-27 23:26:28 +0300

    Factory test added

--- NUnit.Tests/QuadraticEquationFactoryTest.cs
+++ NUnit.Tests/QuadraticEquationFactoryTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Quadratic_Equation.Factories;
 using Quadratic_Equation.Models;
 using Rhino.Mocks;
@@ -13,12 +14,19 @@ namespace NUnit.Tests
         {
             var model = new QuadraticEquationModel() {A = 2, B = 6, C = 2};
 
-            var expectedResult = new QuadraticEquationRoot { D = };
+            var expectedResult = new QuadraticEquationRoot { D = 20, X1 = -0.382, X2 = -2.618};
 
             var factory = new QuadraticEquationFactory();
             var actualResult = factory.CalculateRoot(model);
 
-            Assert.IsTrue(actualResult);
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void QuadraticEquationFactory_CalculateRoot_NullModel()
+        {
+            var factory = new QuadraticEquationFactory();
+            Assert.Throws<ArgumentNullException>(() => factory.CalculateRoot(null));
         }
     }
 }
